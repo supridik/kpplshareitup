501f0d26cf050cc739635e2fb123ef28d083ec6f
sarahfauziyyah <33997307+sarahfauziyyah@users.noreply.github.com>
2017-12-06 15:04:15 +0700

    Update Profile.php

--- application/views/Profile.php
+++ application/views/Profile.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title>Bootstrap E-commerce Templates</title>
+		<title>Profil Anda</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="">
 		<!--[if ie]><meta content='IE=8' http-equiv='X-UA-Compatible'/><![endif]-->
@@ -37,10 +37,8 @@
 				<div class="span8">
 					<div class="account pull-right">
 						<ul class="user-menu">				
-							<li><a href="#">My Account</a></li>
-							<li><a href="cart.html">Your Cart</a></li>
-							<li><a href="checkout.html">Checkout</a></li>					
-							<li><a href="register.html">Login</a></li>			
+							<li><b>Hi, <?php echo $this->session->userdata('nama_user'); ?></b></li>
+							<li><a style="padding: 3px 5px;" class="btn btn-inverse large" href="<?php echo site_url('Logout'); ?>">Logout</a></li>		
 						</ul>
 					</div>
 				</div>
@@ -52,24 +50,25 @@
 					<a href="index.html" class="logo pull-left"><img src="themes/images/logo.png" class="site_logo" alt=""></a>
 					<nav id="menu" class="pull-right">
 						<ul>
-							<li><a href="./products.html">Woman</a>					
-								<ul>
+							<li><a href="<?php echo base_url("home")?>">Home</a>					
+								<!-- <ul>
 									<li><a href="./products.html">Lacinia nibh</a></li>									
 									<li><a href="./products.html">Eget molestie</a></li>
 									<li><a href="./products.html">Varius purus</a></li>									
-								</ul>
+								</ul> -->
 							</li>															
-							<li><a href="./products.html">Man</a></li>			
-							<li><a href="./products.html">Sport</a>
-								<ul>									
+							<li><a href="<?php echo base_url("profile")?>">Profil</a>
+								<!-- <ul>
+									<li><a href="./products.html">Lacinia nibh</a></li>									
+									<li><a href="./products.html">Eget molestie</a></li>
+									<li><a href="./products.html">Varius purus</a></li>									
+								</ul> -->								
+							<li><a href="">Barang</a>
+								<!-- <ul>									
 									<li><a href="./products.html">Gifts and Tech</a></li>
 									<li><a href="./products.html">Ties and Hats</a></li>
 									<li><a href="./products.html">Cold Weather</a></li>
-								</ul>
-							</li>							
-							<li><a href="./products.html">Hangbag</a></li>
-							<li><a href="./products.html">Best Seller</a></li>
-							<li><a href="./products.html">Top Seller</a></li>
+								</ul> -->
 						</ul>
 					</nav>
 				</div>
@@ -78,8 +77,8 @@
 				<iframe width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=74%2F6+Nguy%E1%BB%85n+V%C4%83n+Tho%E1%BA%A1i,+S%C6%A1n+Tr%C3%A0,+%C4%90%C3%A0+N%E1%BA%B5ng,+Vi%E1%BB%87t+Nam&amp;aq=0&amp;oq=74%2F6+Nguyen+Van+Thoai+Da+Nang,+Viet+Nam&amp;sll=37.0625,-95.677068&amp;sspn=41.546728,79.013672&amp;ie=UTF8&amp;hq=&amp;hnear=74+Nguy%E1%BB%85n+V%C4%83n+Tho%E1%BA%A1i,+Ng%C5%A9+H%C3%A0nh+S%C6%A1n,+Da+Nang,+Vietnam&amp;t=m&amp;ll=16.064537,108.24151&amp;spn=0.032992,0.039396&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>
 			</section>
 			<section class="header_text sub">
-			<img class="pageBanner" src="themes/images/pageBanner.png" alt="New products" >
-				<h4><span>Contact Us</span></h4>
+			<img class="pageBanner" src="themes/images/pageBanner.png" alt="<?php echo $this->session->userdata('username'); ?>" >
+				<h4><span>My Profile</span></h4>
 			</section>
 			<section class="main-content">				
 				<div class="row">				
@@ -99,34 +98,16 @@
 						</div>
 					</div>
 					<div class="span7">
-						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-						<form method="post" action="#">
-							<fieldset>
-								<div class="clearfix">
-									<label for="name"><span>Name:</span></label>
-									<div class="input">
-										<input tabindex="1" size="18" id="name" name="name" type="text" value="" class="input-xlarge" placeholder="Name">
-									</div>
-								</div>
-								
-								<div class="clearfix">
-									<label for="email"><span>Email:</span></label>
-									<div class="input">
-										<input tabindex="2" size="25" id="email" name="email" type="text" value="" class="input-xlarge" placeholder="Email Address">
-									</div>
-								</div>
-								
-								<div class="clearfix">
-									<label for="message"><span>Message:</span></label>
-									<div class="input">
-										<textarea tabindex="3" class="input-xlarge" id="message" name="body" rows="7" placeholder="Message"></textarea>
-									</div>
-								</div>
-								
-								<div class="actions">
-									<button tabindex="3" type="submit" class="btn btn-inverse">Send message</button>
-								</div>
-							</fieldset>
+						<p>Nama Lengkap</p>
+						<h4><?php echo $this->session->userdata('nama_user'); ?></h4>
+						<p>Alamat</p>
+						<h4><?php echo $this->session->userdata('alamat');?>, <?php echo $this->session->userdata('kelurahan');?>, <?php echo $this->session->userdata('kecamatan'); ?></h4>
+						<p>Kode Pos</p>
+						<h4><?php echo $this->session->userdata('kode_pos');?></h4>
+						<p>Deskripsi</p>
+						<h4><?php echo $this->session->userdata('deskripsi');?></h4>
+						
+						
 						</form>
 					</div>				
 				</div>
